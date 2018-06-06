26e134fd07d5e03ce07a76e6ab9b505ccb4efeed
iggdias <dias.igor.ti@gmail.com>
2018-06-05 23:36:35 -0300

    Testando arrays e referências

---
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteArrayDePrimitivos.java
@@ -0,0 +1,18 @@
+package br.com.bytebank.banco.test;
+
+public class TesteArrayDePrimitivos {
+
+	public static void main(String[] args) {
+		
+		int[] idades = new int[5];
+		idades[0] = 23;
+		idades[1] = 32;
+		idades[2] = 45;
+		idades[3] = 65;
+		idades[4] = 86;
+		
+		int idade3 = idades[2];
+		System.out.println("idade 3: " + idade3);
+		
+	}
+}
---
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteString.java
@@ -0,0 +1,24 @@
+package br.com.bytebank.banco.test;
+
+public class TesteString {
+
+	public static void main(String[] args) {
+
+		String nome = "Igor";
+		String outroNome = new String("Dias");
+		
+		System.out.println("nome = " + nome + ", outro nome = " + outroNome);
+		System.out.println(nome.replace("g", "x").toUpperCase() );
+		
+		System.out.println(outroNome.charAt(2));
+		
+		System.out.println(nome.indexOf("gor"));
+		
+		System.out.println(outroNome.substring(0));
+		
+		for(int i = 0; i < outroNome.length(); i++) {
+			System.out.println(outroNome.charAt(i));
+		}
+	}
+
+}
