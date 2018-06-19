8bfc42c904c2b76b68fd51db25f34313af6e576a
iggdias <dias.igor.ti@gmail.com>
2018-06-19 01:07:29 -0300

    Testando wrappers para primitivos

---
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/TesteWrapperInteger.java
@@ -0,0 +1,35 @@
+package br.com.bytebank.banco.test.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TesteWrapperInteger {
+
+	public static void main(String[] args) {
+		
+		Integer idadeRef = 29;
+		int primitivo = new Integer(21);
+
+		List<Integer> lista = new ArrayList<>();
+		lista.add(idadeRef);
+		lista.add(primitivo);
+		
+		int i1 = lista.get(0);
+		Integer i2 = lista.get(1);	
+		
+		System.out.println(i1);
+		System.out.println(i2);
+		
+		Integer valorRef = Integer.valueOf(33);
+		int valorPrimitivo = valorRef.intValue();
+		
+		System.out.println(valorPrimitivo);
+		
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Integer.BYTES);
+		System.out.println(Integer.SIZE);
+		
+	}
+
+}
